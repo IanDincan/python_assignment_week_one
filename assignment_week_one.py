fe95c1e6dd9c159b5021953613104c32091d5850
IanDincan <ianwandui@gmail.com>
2025-04-06 19:20:13 +0300

    Add assignment_week_one.py

---
+++ assignment_week_one.py
@@ -0,0 +1,19 @@
+first_number=input("Give me a number ")
+second_number=input("Give me another number " )
+Operator=input("What operator do you want to use ")
+if operator="+":
+    sum=int(first_number)+int(second_number)
+    print(f"{first_number}+{second_number}={sum}")
+elif operator="-":
+    answer=int(first_number)-int(second_number)
+    print(f"{first_number}-{second_number}={answer}")
+elif operator="*":
+    product=int(first_number)*int(second_number)
+    print(f"{first_number}*{second_number}={product}")
+elif operator="/":
+    quotient=int(first_number)/int(second_number)
+    print(f"{first_number}/{second_number}={quotient}")
+elif operator="**":
+    power=int(first_number)**int(second_number)
+    print(f"{first_number}**{second_number}={power}")       
+
